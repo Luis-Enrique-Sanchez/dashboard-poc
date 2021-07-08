60c80d3325abc92dbd522557b686a7cad9af59b2
Luis-Enrique-Sanchez <luis.sanchezs@agilethought.com>
2021-07-08 15:30:11 -0500

    Cambios a index componentn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { FormsModule } from '@angular/forms'
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { IndexComponent } from './index/index.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
