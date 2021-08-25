a13c10e0af123b44e11614e3ac5f0355c14d680c
YozgartGarcia <yozgart.garcia@agilethought.com>
2021-08-25 09:48:13 -0500

    Environment changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api_url:"http://ec2-3-81-147-145.compute-1.amazonaws.com:7777",
-  getItemsPath:"/portalserver/services/rest/api/v1/private/get/items",
+  getItemsPath:"/portalserver/services/rest/api/v1/private/get/items/online",
   backbase_url:"http://ec2-3-81-147-145.compute-1.amazonaws.com:7777",
   logOut_url:"/portalserver/test-dashboard/index"
 };
